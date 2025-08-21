e475f7884aa2ba9f9ac2ff75b2b4fd651bf7ed7c
SeluneApp <SeluneApp@gmail.com>
2025-08-20 22:50:00 -0300

    Commit né

---
+++ db.js
@@ -0,0 +1,13 @@
+const mariadb = require('mariadb');
+
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'client_order_system',
+  port: 3306,
+  connectionLimit: 10,
+  bigintAsNumber: true
+});
+
+module.exports = pool;
